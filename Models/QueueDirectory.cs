58fd32b38e9f08f3b3daf831df308e89d9d4c60a
AlexSolovey <sania.saw@gmail.com>
2023-02-09 10:51:22 +0300

    removed unused

--- Models/QueueDirectory.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DirectorySearcherTestApp.Models
-{
-    public class QueueDirectory
-    {
-        public string Path { get; set; }
-        public ResultNode ResultNode { get; set; }
-    }
-}
